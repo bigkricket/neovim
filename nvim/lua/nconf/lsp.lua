07336fa357428ca405a44fa723a362a6ec5415f6
Ruben Perez <ruben.perez@flyzipline.com>
2026-06-21 17:38:51 -0700

    auto suggestions

--- nvim/lua/nconf/lsp.lua
+++ nvim/lua/nconf/lsp.lua
@@ -10,12 +10,13 @@ return {
       "neovim/nvim-lspconfig",
     },
     opts = {
-      ensure_installed = { "rust_analyzer" },
+      ensure_installed = { "rust_analyzer", "clangd" },
     },
   },
   {
   "neovim/nvim-lspconfig",
     config = function()
+      vim.opt.completeopt = { "menuone", "noselect", "popup" }
       vim.lsp.inlay_hint.enable(true)
 
       vim.lsp.config("rust_analyzer", {
@@ -42,8 +43,11 @@ return {
           },
         },
       })
+
+      vim.lsp.config("clangd", {})
   
       vim.lsp.enable("rust_analyzer")
+      vim.lsp.enable("clangd")
   
       local inlay_group = vim.api.nvim_create_augroup("nconf_lsp_inlay", { clear = true })
 
@@ -51,6 +55,13 @@ return {
         group = inlay_group,
         callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+	  if client then
+	    vim.lsp.completion.enable(true, client.id, args.buf, {
+	      autotrigger = true,
+	    })
+	  end
+	  
           if client and client:supports_method("textDocument/inlayHint") then
             vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
           end
@@ -58,6 +69,18 @@ return {
       })
   
       -- keymaps
+      vim.keymap.set("i", "<C-Space>", function()
+  	vim.lsp.completion.get()
+      end)
+
+      vim.keymap.set("i", "<Tab>", function()
+	return vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>"
+      end, { expr = true })
+
+      vim.keymap.set("i", "<S-Tab>", function()
+	return vim.fn.pumvisible() == 1 and "<C-p>" or "<S-Tab>"
+      end, { expr = true })
+
       vim.keymap.set("n", "gd", vim.lsp.buf.definition)
       vim.keymap.set("n", "K", vim.lsp.buf.hover)
       vim.keymap.set("n", "gr", vim.lsp.buf.references)
